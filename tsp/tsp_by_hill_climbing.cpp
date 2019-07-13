699d32b8156a45bdd6c4c8e0557130ad076b82aa
NaotoYamaguchi <utyamaguchi59@gmail.com>
2019-07-13 12:38:21 +0900

    add: c++ program and fix a svg file suffix

--- tsp/tsp_by_hill_climbing.cpp
+++ tsp/tsp_by_hill_climbing.cpp
@@ -1,54 +1,127 @@
-<svg width="10000" height="10000">
-	<line x1="580" y1="1175" x2 ="650" y2="1130" stroke="black" />
-	<line x1="650" y1="1130" x2 ="525" y2="1000" stroke="black" />
-	<line x1="525" y1="1000" x2 ="475" y2="960" stroke="black" />
-	<line x1="475" y1="960" x2 ="510" y2="875" stroke="black" />
-	<line x1="510" y1="875" x2 ="555" y2="815" stroke="black" />
-	<line x1="555" y1="815" x2 ="760" y2="650" stroke="black" />
-	<line x1="760" y1="650" x2 ="845" y2="655" stroke="black" />
-	<line x1="845" y1="655" x2 ="880" y2="660" stroke="black" />
-	<line x1="880" y1="660" x2 ="945" y2="685" stroke="black" />
-	<line x1="945" y1="685" x2 ="975" y2="580" stroke="black" />
-	<line x1="975" y1="580" x2 ="1220" y2="580" stroke="black" />
-	<line x1="1220" y1="580" x2 ="1320" y2="315" stroke="black" />
-	<line x1="1320" y1="315" x2 ="1250" y2="400" stroke="black" />
-	<line x1="1250" y1="400" x2 ="1340" y2="725" stroke="black" />
-	<line x1="1340" y1="725" x2 ="1605" y2="620" stroke="black" />
-	<line x1="1605" y1="620" x2 ="1740" y2="245" stroke="black" />
-	<line x1="1740" y1="245" x2 ="1530" y2="5" stroke="black" />
-	<line x1="1530" y1="5" x2 ="1465" y2="200" stroke="black" />
-	<line x1="1465" y1="200" x2 ="1215" y2="245" stroke="black" />
-	<line x1="1215" y1="245" x2 ="1170" y2="65" stroke="black" />
-	<line x1="1170" y1="65" x2 ="660" y2="180" stroke="black" />
-	<line x1="660" y1="180" x2 ="560" y2="365" stroke="black" />
-	<line x1="560" y1="365" x2 ="480" y2="415" stroke="black" />
-	<line x1="480" y1="415" x2 ="420" y2="555" stroke="black" />
-	<line x1="420" y1="555" x2 ="345" y2="750" stroke="black" />
-	<line x1="345" y1="750" x2 ="145" y2="665" stroke="black" />
-	<line x1="145" y1="665" x2 ="300" y2="465" stroke="black" />
-	<line x1="300" y1="465" x2 ="95" y2="260" stroke="black" />
-	<line x1="95" y1="260" x2 ="25" y2="230" stroke="black" />
-	<line x1="25" y1="230" x2 ="25" y2="185" stroke="black" />
-	<line x1="25" y1="185" x2 ="410" y2="250" stroke="black" />
-	<line x1="410" y1="250" x2 ="595" y2="360" stroke="black" />
-	<line x1="595" y1="360" x2 ="725" y2="370" stroke="black" />
-	<line x1="725" y1="370" x2 ="830" y2="485" stroke="black" />
-	<line x1="830" y1="485" x2 ="830" y2="610" stroke="black" />
-	<line x1="830" y1="610" x2 ="835" y2="625" stroke="black" />
-	<line x1="835" y1="625" x2 ="845" y2="680" stroke="black" />
-	<line x1="845" y1="680" x2 ="1150" y2="1160" stroke="black" />
-	<line x1="1150" y1="1160" x2 ="875" y2="920" stroke="black" />
-	<line x1="875" y1="920" x2 ="795" y2="645" stroke="black" />
-	<line x1="795" y1="645" x2 ="770" y2="610" stroke="black" />
-	<line x1="770" y1="610" x2 ="720" y2="635" stroke="black" />
-	<line x1="720" y1="635" x2 ="565" y2="575" stroke="black" />
-	<line x1="565" y1="575" x2 ="520" y2="585" stroke="black" />
-	<line x1="520" y1="585" x2 ="415" y2="635" stroke="black" />
-	<line x1="415" y1="635" x2 ="575" y2="665" stroke="black" />
-	<line x1="575" y1="665" x2 ="605" y2="625" stroke="black" />
-	<line x1="605" y1="625" x2 ="685" y2="610" stroke="black" />
-	<line x1="685" y1="610" x2 ="685" y2="595" stroke="black" />
-	<line x1="685" y1="595" x2 ="700" y2="580" stroke="black" />
-	<line x1="700" y1="580" x2 ="700" y2="500" stroke="black" />
-	<line x1="700" y1="500" x2 ="580" y2="1175" stroke="blue" />
-</svg>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+random_device seed_gen;
+mt19937 engine(seed_gen());
+
+int dis(pair<int, int> i, pair<int, int> j) {
+	return sqrt(pow((i.first - j.first), 2) + pow((i.second - j.second), 2));
+}
+
+float score(vector<pair<int, int>> cities, vector<int> route) {
+	float score = 0;
+	for(int i=0; i<route.size()-1; i++) {
+		score += dis(cities[route[i]], cities[route[i+1]]);
+	}
+	return score;
+}
+
+void output_svg(vector<pair<int, int>> cities, vector<int> route) {
+	string filename = "tsp_by_hill_climbing_O3.html";
+	ofstream writing_file;
+	writing_file.open(filename, ios::out);
+	int width = 10000;
+	int height = 10000;
+	writing_file << "<svg width=\"" << width << "\" height=\"" << height << "\">" << "\n\t";
+
+	for(int i=0; i<route.size()-1; i++) {
+		float x1 = cities[route[i]].first;
+		float y1 = cities[route[i]].second;
+		float x2 = cities[route[i+1]].first;
+		float y2 = cities[route[i+1]].second;
+		writing_file << "<line x1=\"" << x1 << "\" y1=\"" << y1 << "\" x2 =\"" << x2 << "\" y2=\"" << y2 << "\" stroke=\"black\" />" << "\n\t";
+	}
+
+	float sx = cities[route[0]].first;
+	float sy = cities[route[0]].second;
+	float gx = cities[route[route.size()-1]].first;
+	float gy = cities[route[route.size()-1]].second;
+	writing_file << "<line x1=\"" << gx << "\" y1=\"" << gy << "\" x2 =\"" << sx << "\" y2=\"" << sy << "\" stroke=\"blue\" />";
+
+	writing_file << "\n</svg>";
+
+}
+
+int main(void) {
+	vector<pair<int, int>> cities;
+	int index;
+	float x, y;
+	vector<int> initial_route;
+	while(cin >> index >> x >> y) {
+		pair<int, int> p = make_pair(x, y);
+		cities.push_back(p);
+		initial_route.push_back(index-1);
+	}
+
+	ofstream outputfile("hill_climbing_O3.csv", ios::app);
+	int total_best_score = 1e10;
+	vector<int> total_best_route;
+
+	auto start = chrono::system_clock::now();
+	for(int i=0; i<1000; i++) {
+		cout << i << "\n";
+		shuffle(initial_route.begin(), initial_route.end(), engine);
+		//for(auto i:initial_route) {
+		//	cout << i << ":";
+		//}
+
+		//cout << "\n";
+
+	//	for(auto p:cities) {
+	//		cout << p.first << ":" << p.second << "\n";
+	//	}
+	//	for(auto i:initial_route) {
+	//		cout << i << "\n"; 
+	//	}
+		//cout << score(cities, initial_route) << "\n";
+
+		float best_score = score(cities, initial_route);
+		vector<int> best_route = initial_route;
+		bool updated = true;
+		
+		while(updated) {
+			for(int i=0; i<best_route.size()-1; i++) {
+				for(int j=0; j<best_route.size()-1; j++) {
+					if(i==j) continue;
+						
+					vector<int> route = best_route;
+					int tmp = route[i]; route[i] = route[j]; route[j] = tmp;
+					float tmp_score = score(cities, route);
+					updated = false;
+					if(best_score > tmp_score) {
+						best_route = route;
+						best_score = tmp_score;
+						//cout << best_score << "\n";
+						updated = true;
+					}
+				}
+			}
+		}
+
+		outputfile << best_score << "\n";
+		//cout << "route:" << "\n";
+		if(total_best_score >= best_score) {
+			total_best_score = best_score;
+			total_best_route = best_route;
+		}
+
+	}
+	auto end = chrono::system_clock::now();
+	auto dur = end - start;
+	auto msec = chrono::duration_cast<chrono::milliseconds>(dur).count();
+    // 要した時間をミリ秒（1/1000秒）に変換して表示
+    cout << msec << " milli sec \n";
+
+	cout << total_best_score << "\n";
+	output_svg(cities, total_best_route);
+	outputfile.close();
+
+}
+// 初期値をランダムに1000回くらい
+// 近傍の決め方
